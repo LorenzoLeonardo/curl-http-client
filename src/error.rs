1b7ce7c62378f0ffb6d222239374a67fb0a14472
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2023-10-29 05:21:46 +0800

    impl Display and std::error::Error for Error

--- src/error.rs
+++ src/error.rs
@@ -10,3 +10,16 @@ pub enum Error {
     Perform(async_curl::error::Error<Collector>),
     Other(String),
 }
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Error::Curl(err) => write!(f, "{}", err),
+            Error::Http(err) => write!(f, "{}", err),
+            Error::Perform(err) => write!(f, "{}", err),
+            Error::Other(err) => write!(f, "{}", err),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
