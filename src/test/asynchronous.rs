7be0dd58e8792d08f5820ab8f450ada9fe897458
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2026-06-27 09:50:32 +0800

    Added interface to perform curl easy2
    In the new async-curl crate, additional option is
    offered to use the curl easy2 perform aside from the existing curl
    multi.
    
    Users can now choose which transfer they want to use.

--- src/test/asynchronous.rs
+++ src/test/asynchronous.rs
@@ -59,3 +59,56 @@ async fn test_across_multiple_threads() {
         }
     }
 }
+
+#[tokio::test]
+async fn test_across_multiple_threads_using_easy2() {
+    let responder = MockResponder::new(ResponderType::Body("test body".as_bytes().to_vec()));
+    let (server, _tempdir) = setup_test_environment(responder).await;
+    let target_url = Url::parse(format!("{}/test", server.uri()).as_str()).unwrap();
+
+    let curl = CurlActor::new();
+    let collector = Collector::Ram(Vec::new());
+    let request = Request::builder()
+        .uri(target_url.as_str())
+        .method(Method::GET)
+        .body(None)
+        .unwrap();
+
+    const NUM_CONCURRENT: usize = 100;
+
+    let mut handles = Vec::new();
+    for _n in 0..NUM_CONCURRENT {
+        let curl = curl.clone();
+        let collector = collector.clone();
+        let request = request.clone();
+        let handle = tokio::spawn(async move {
+            let response = HttpClient::new(collector)
+                .request(request)
+                .unwrap()
+                .nonblocking(curl)
+                .perform_easy2()
+                .await
+                .unwrap();
+            println!("Response: {:?}", response);
+            assert_eq!(response.status(), StatusCode::OK);
+            assert_eq!(
+                *response.body().as_ref().unwrap(),
+                "test body".as_bytes().to_vec()
+            );
+        });
+        handles.push(handle);
+    }
+
+    let results: Vec<Result<_, _>> = future::join_all(handles).await;
+
+    for (i, result) in results.into_iter().enumerate() {
+        match result {
+            Ok(res) => {
+                println!("Task {} result: {:?}", i + 1, res);
+            }
+            Err(e) => {
+                panic!("{}", e);
+            }
+        }
+    }
+}
